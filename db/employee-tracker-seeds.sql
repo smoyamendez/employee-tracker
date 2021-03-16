20a6517daf62747a2985cafc67dc8db74b4c2004
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-03-15 23:24:36 -0400

    department seeds & role seeds added

--- db/employee-tracker-seeds.sql
+++ db/employee-tracker-seeds.sql
@@ -26,6 +26,8 @@ CREATE TABLE role (
 
 -- CREATING EMPLOYEE TABLE --
 CREATE TABLE employee (
+    -- CREATE ID COLUMN --
+    id INT NOT NULL AUTO_INCREMENT,
     -- CREATE FIRST_NAME COLUMN --
     first_name VARCHAR(30) NOT NULL,
     -- CREATE LAST_NAME COLUMN --
@@ -33,10 +35,31 @@ CREATE TABLE employee (
     -- CREATE ROLE_ID COLUMN --
     role_id INT NOT NULL,
     -- CREATE MANAGER_ID COLUMN --
-    manager_id INT ,
+    manager_id INT,
+     -- MAKES ID PRIMARY KEY --
+    PRIMARY KEY (id),
     -- MAKES ROLE_ID FOREIGN KEY --
     FOREIGN KEY (role_id) REFERENCES role(id),
     -- MAKES MANAGER_ID FOREIGN KEY --
-    FOREIGN KEY (manager_id) REFERENCES role(id)
-)
+    -- FIXME: may need to be role(id) --
+    FOREIGN KEY (manager_id) REFERENCES employee(id) 
+);
  
+
+-- DEPARTMENT SEEDS --
+INSERT INTO department (name)
+VALUES ("Management"), ("Sales"), ("Accounting"), ("Human Resources"), ("Reception"), ("Customer Relations"), ("Temp Employee");
+
+-- ROLE SEEDS --
+INSERT INTO role (title, salary, department_id)
+VALUES ('Regional Manager', 60000.00, 1), ('Assistant (to the) Regional Manager', 68000.00, 2) ('Salesperson', 65000.00, 3), 
+('Lead Accountant', 70000.00, 4), ('Accountant', 60000.00, 5),('Receptionist', 40000.00, 6), ('Quality Control', 45000.00, 7), 
+('Supplier Relations', 45000.00, 8), ('Human Resources', 65000.00, 9), ('Customer Relations Specialist', 42000.00, 10), ('Temp', 38000.00, 11);
+
+-- EMPLOYEE SEEDS --
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Michael", "Scott", 1, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dwight", "Schrute", 2);
+
